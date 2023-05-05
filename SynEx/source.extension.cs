458965d2cc53fc979120fa0ea988d39c38f147e8
jojomondag <josef.nobach@outlook.com>
2023-05-05 06:30:22 +0200

    Small Changes
    Update Icon background color, added some better desciption.

--- SynEx/source.extension.cs
+++ SynEx/source.extension.cs
@@ -9,7 +9,7 @@ namespace SynEx
     {
         public const string Id = "SynEx_SyntaxExtractor";
         public const string Name = "SynEx";
-        public const string Description = @"Syntax Extractor: Enhance Your AI-Code Interaction";
+        public const string Description = @"The Syntax Extractor is an extension that allows you to extract the information about the names of classes and functions in your project. This tool makes it easier to communicate your context and scope of your currently opened project. The extracted information is automatically saved as text documents and clipboard data, making it easy to share with other developers or AI language models. Whether you're working on a small project or a large-scale application, the Syntax Extractor can help you streamline your workflow and save time.";
         public const string Language = "en-US";
         public const string Version = "1.0";
         public const string Author = "Josef Nobach";
