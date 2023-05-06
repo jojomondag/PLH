087d530c5d134ebdef7c073901b7262b09e0eb17
jojomondag <josef.nobach@outlook.com>
2023-05-06 21:29:28 +0200

    Small cleanup added Dll's
    Small cleanup added Assemblies so anybody can use the extention.

--- SynEx/SynExPackage.cs
+++ SynEx/SynExPackage.cs
@@ -4,6 +4,8 @@ global using System;
 global using Task = System.Threading.Tasks.Task;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.IO;
+using System.Reflection;
 
 namespace SynEx
 {
@@ -16,9 +18,39 @@ namespace SynEx
     {
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
             await this.RegisterCommandsAsync();
             await SynExMainWindowCommand.InitializeAsync(this);
             UserControl.Initialize(this);
+        }
+
+        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            var assemblyName = new AssemblyName(args.Name);
+            string assemblyFileName = null;
+
+            if (assemblyName.Name == "Microsoft.CodeAnalysis.CSharp")
+            {
+                assemblyFileName = "Microsoft.CodeAnalysis.CSharp.dll";
+            }
+            else if (assemblyName.Name == "Microsoft.CodeAnalysis")
+            {
+                assemblyFileName = "Microsoft.CodeAnalysis.dll";
+            }
+
+            if (assemblyFileName != null)
+            {
+                string extensionDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                string assemblyPath = Path.Combine(extensionDir, "Assemblys", assemblyFileName);
+
+                if (File.Exists(assemblyPath))
+                {
+                    return Assembly.LoadFrom(assemblyPath);
+                }
+            }
+
+            return null;
         }
     }
 }
--- SynEx/source.extension.cs
+++ SynEx/source.extension.cs
@@ -7,12 +7,12 @@ namespace SynEx
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "SynEx_SyntaxExtractor_1.0";
-        public const string Name = "SynEx";
+        public const string Id = "SynEx_SyntaxExtractor_1.02";
+        public const string Name = "Syntax Extractor";
         public const string Description = @"The Syntax Extractor is an extension that allows you to extract the information about the names of classes and functions in your project. This tool makes it easier to communicate your context and scope of your currently opened project. The extracted information is automatically saved as text documents and clipboard data, making it easy to share with other developers or AI language models. Whether you're working on a small project or a large-scale application, the Syntax Extractor can help you streamline your workflow and save time.";
         public const string Language = "en-US";
-        public const string Version = "1.0";
+        public const string Version = "1.2";
         public const string Author = "Josef Nobach";
-        public const string Tags = "Ai, Syntax Extractor, Context, FilePath extractor, EfficientWorkflow, CodeAnalysis";
+        public const string Tags = "Ai, Syntax Extractor, Context, FilePath Extractor, EfficientWorkflow, CodeAnalysis";
     }
 }
